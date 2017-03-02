4990c4f40e4fb0453b4f9b95f29a6c283e08fb34
Jeff Namenye <jeffnamenye@hotmail.com>
2017-03-02 16:58:18 -0500

    fixed issues with program

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-//this sets up my contant for express and body-parser
+// sets up contant for express and body-parser
 const express = require('express');
 const body_parser = require('body-parser');
 
-//this sets up the express functionality
+// express functionality
 const app = express();
 
-//this sets my port to 3000
+// port to 3000
 const port = 3000;
 
 //sets body-parser to json
 app.use(body_parser.json());
 
-//sets the body-parser as url encoded data
+//sets body-parser as url encoded data
 app.use(body_parser.urlencoded({
   extended: true,
 }));
@@ -19,7 +19,7 @@ app.use(body_parser.urlencoded({
 //links routes
 app.use('/api/v1.0.0', require('../routes/api')(express));
 
-//sets my server to listen on port 3000
+// server to listen on port 3000
 const server = app.listen(port, () =>{
   console.log('Good to go on port ' +port);
 });
