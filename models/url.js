bc01a84ceb3460ab377b98e575395738aceb4250
Jeff Namenye <jeffnamenye@hotmail.com>
2017-03-02 18:33:16 -0500

    finished with the read me file

--- models/url.js
+++ models/url.js
@@ -2,7 +2,7 @@
 exports.Url_Short = (link)=>{
   var abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
   var URL_text_length = 7;
-  var URL_text = 'hoitytoity.com';
+  var URL_text = 'http://hoitytoity.com/';
 
   for(var i = 0; i < URL_text_length; i++){
     URL_text += abc.charAt((Math.random()) * abc.length);
